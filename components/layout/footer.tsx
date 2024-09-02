7d12d0e40fcabc15e165a274e9c4814018b2cdd9
TalPiven13 <157593149+TalPiven13@users.noreply.github.com>
2024-09-02 15:24:28 -0700

    fix footer initial result

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,27 +1,27 @@
 import Link from "next/link";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 
+const links = [
+  { href: "#", text: "Terms of Service" },
+  { href: "#", text: "Privacy" },
+] as const;
+
 export default function Footer() {
+  const footerTextClass = "font-mono text-gray-400 text-xs";
   return (
     <footer className="flex sm:flex-row flex-col items-center gap-2 border-gray-800 px-4 md:px-6 py-6 border-t w-full shrink-0">
-      <p className="font-mono text-gray-400 text-xs">
-        © 2023 DevGenius. All rights reserved.
-      </p>
+      <p className={footerTextClass}>© 2023 DevGenius. All rights reserved.</p>
       <nav className="flex gap-4 sm:gap-6 sm:ml-auto">
-        <Link
-          className="font-mono text-gray-400 text-xs hover:text-green-500 underline-offset-4 hover:underline"
-          href="#"
-        >
-          Terms of Service
-        </Link>
-        <Link
-          className="font-mono text-gray-400 text-xs hover:text-green-500 underline-offset-4 hover:underline"
-          href="#"
-        >
-          Privacy
-        </Link>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={`${footerTextClass} hover:text-primary underline-offset-4 hover:underline`}
+          >
+            {link.text}
+          </Link>
+        ))}
       </nav>
-      <ThemeSwitcher />
     </footer>
   );
 }
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -5,6 +5,7 @@ import { Code, Menu, X } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { motion, AnimatePresence } from "framer-motion";
+import { ThemeSwitcher } from "../theme-switcher";
 
 const links = [
   { href: "#services", label: "Services" },
@@ -18,7 +19,7 @@ export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="flex items-center border-gray-800 px-4 lg:px-6 border-b h-14">
+    <header className="flex items-center gap-2 border-gray-800 px-4 lg:px-6 border-b h-14">
       <Link className="flex justify-center items-center" href="#">
         <Code className="w-6 h-6 text-primary" />
         <span className="ml-2 font-bold font-mono text-primary">DevGenius</span>
@@ -35,6 +36,9 @@ export default function Header() {
           </Link>
         ))}
       </nav>
+      <div className="md:block hidden">
+        <ThemeSwitcher />
+      </div>
 
       <Button
         className="md:hidden ml-auto"
@@ -58,23 +62,22 @@ export default function Header() {
             exit={{ x: "100%" }}
             transition={{ duration: 0.3 }}
           >
-            <Button
+            <div
+              className="fixed inset-0 bg-black bg-opacity-50"
               onClick={() => setIsMenuOpen(false)}
-              size="icon"
-              variant="ghost"
-            >
-              <X className="w-6 h-6 text-primary" />
-            </Button>
-            {links.map(({ href, label }) => (
-              <Link
-                key={`${href}${label}`}
-                href={href}
-                className="font-medium text-sm hover:text-primary transition-colors"
-                onClick={() => setIsMenuOpen(false)}
-              >
-                {label}
-              </Link>
-            ))}
+            />
+            <div className="relative z-10 flex flex-col items-end">
+              {links.map(({ href, label }) => (
+                <Link
+                  key={`${href}${label}`}
+                  href={href}
+                  className="block text-right py-2 font-medium text-sm hover:text-primary transition-colors"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  {label}
+                </Link>
+              ))}
+            </div>
           </motion.div>
         )}
       </AnimatePresence>
