689e5d5f39d621723621fbd86b0196768ed7feed
TalPiven13 <157593149+TalPiven13@users.noreply.github.com>
2024-11-04 19:34:28 +0200

    fix companies to match colors

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -6,23 +6,12 @@ const links = [
 ] as const;
 
 export default function Footer() {
-  const footerTextClass = "font-mono text-gray-400 text-xs";
+  const footerTextClass = "font-mono text-gray-400 text-xs w-full";
   return (
-    <footer className="flex sm:flex-row flex-col items-center gap-2 dark:border-gray-800 px-4 md:px-6 py-6 border-t w-full shrink-0">
+    <footer className="flex sm:flex-row flex-col items-center gap-2 dark:border-gray-800 px-4 md:px-6 py-6 border-t w-full text-center shrink-0">
       <p className={footerTextClass}>
         © 2024 Elfo Software LLC. All rights reserved.
       </p>
-      <nav className="flex items-center gap-4 sm:gap-6 sm:ml-auto">
-        {links.map((link) => (
-          <Link
-            key={link.id}
-            href={link.href}
-            className={`${footerTextClass} hover:text-primary underline-offset-4 hover:underline`}
-          >
-            {link.text}
-          </Link>
-        ))}
-      </nav>
     </footer>
   );
 }
